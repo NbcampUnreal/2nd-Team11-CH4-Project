effd6abe92e20ffb0faad8ffdb44dc7f5b443455
SeonGuk Kim <tjsrnr3180@gmail.com>
2025-04-16 12:04:38 +0900

    [bugfix] 타이틀 레벨에서 로컬플레이어 컨트롤러만 UI를 생성하도록 수정

--- SSBProject/Source/SSBProject/PlayerControllers/TitleWidgetController.cpp
+++ SSBProject/Source/SSBProject/PlayerControllers/TitleWidgetController.cpp
@@ -6,12 +6,12 @@
 void ATitleWidgetController::BeginPlay()
 {
 	Super::BeginPlay();
-
-	if (true == ::IsValid(UIWidgetClass))
+	
+	if (IsLocalController() && true == IsValid(UIWidgetClass))
 	{
 		UIWidgetInstance = CreateWidget<UUserWidget>(this, UIWidgetClass);
 		// CreateWidget()이 호출될 때 UIWidgetInstance->NativeOnInitialize() 함수가 호출됨.
-		if (true == ::IsValid(UIWidgetInstance))
+		if (UIWidgetInstance)
 		{
 			UIWidgetInstance->AddToViewport();
 			// AddToViewport()가 호출 될 때 UIWidgetInstance->NativeConstruct() 함수가 호출됨.
